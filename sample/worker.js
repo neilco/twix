efa819e180a56eb3a1610e1897ec862a75c71b0b
Neil Cowburn <neilco@gmail.com>
2020-10-23 17:07:39 +0100

    Update sample

--- sample/worker.js
+++ sample/worker.js
@@ -3,9 +3,9 @@ importScripts('../twix.min.js');
 getTime();
 setInterval(getTime, 1000);
 
-function getTime() { 
+function getTime() {
     Twix.ajax({
-        url: "https://neilcowburn.com/api/time.php",
+        url: "https://neilcowburn.com/api/test.php",
         success: function(data) {
             postMessage(data);
         }
